df634788cd1a10c8238e96e15196de38626539a6
Nikhil Khot <nikhilkhot976@gmail.com>
2025-03-23 13:15:50 +0530

    Created Message Routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import authRoutes from './routes/auth.route.js'
 import dotenv from 'dotenv'
 import cookieParser from "cookie-parser"
+import messageRoutes from "./routes/message.route.js"
 
 const app = express()
 import {connectDB} from './lib/db.js'
@@ -12,6 +13,7 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 
 app.listen(PORT, () =>{
