cd72bbc341e80bac924ba6a2239e623b4f6fc25a
Nikhil Khot <nikhilkhot976@gmail.com>
2025-03-23 08:00:13 +0530

    Implemented Auth

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import authRoutes from './routes/auth.route.js'
+import dotenv from 'dotenv'
+import cookieParser from "cookie-parser"
+
+const app = express()
+import {connectDB} from './lib/db.js'
+
+dotenv.config()
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth", authRoutes)
+
+
+app.listen(PORT, () =>{
+    console.log(`server is running on port ${PORT}`);
+    connectDB()
+})
