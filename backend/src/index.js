6bce422943ff19510bcda7744542131c7c96c934
Nikhil Khot <nikhilkhot976@gmail.com>
2025-03-26 21:01:36 +0530

    created signup and login pages

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import authRoutes from './routes/auth.route.js'
 import dotenv from 'dotenv'
 import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.route.js"
+import cors from 'cors'
 
 const app = express()
 import {connectDB} from './lib/db.js'
@@ -12,6 +13,11 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true,
+}
+))
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 
